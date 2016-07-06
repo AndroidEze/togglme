e040ea86d92c9325f58ed042e95e850ed3371de9
Ezequiel García <ezeezegg@gmail.com>
2016-07-05 22:56:00 -0300

    add method in volley for make requests

--- app/src/main/java/me/ezeezegg/togglme/helpers/VolleyHelper.java
+++ app/src/main/java/me/ezeezegg/togglme/helpers/VolleyHelper.java
@@ -2,9 +2,15 @@ package me.ezeezegg.togglme.helpers;
 
 import android.content.Context;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 import org.json.JSONObject;
 
 import me.ezeezegg.togglme.Interfaces.AsyncVolleyResponse;
+import me.ezeezegg.togglme.applications.VolleyApplication;
 
 /**
  * Created by egarcia on 7/5/16.
@@ -20,5 +26,21 @@ public class VolleyHelper {
         this.url = url;
         this.jsonObject = jsonObject;
         this.asyncVolleyResponse = asyncVolleyResponse;
+    }
+
+    public void makeVolleyRequest(){
+        JsonObjectRequest request = new JsonObjectRequest(url, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        VolleyApplication.getInstance().getRequestQueue().add(request);
     }
 }
